750006c9d55bf4608fcd57947c0b3d923cfcd8b8
rafael-the-dev <luislangabusiness@gmail.com>
2026-06-03 08:29:59 +0200

    [FIX] middleware file name convetions is drepecated, So I replace it by proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,5 +1,8 @@
-import { auth } from "@/server/auth";
+import NextAuth from "next-auth";
+import { authConfig } from "@/server/auth/config";
 import { NextResponse } from "next/server";
+
+const { auth } = NextAuth(authConfig);
 
 const PUBLIC_PATHS = ["/login", "/register", "/api/auth"];
 
