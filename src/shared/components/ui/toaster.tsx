eecb4ce11b6c385b0edceba40863df9ed4af4f07
rafael-the-dev <luislangabusiness@gmail.com>
2026-06-03 08:30:55 +0200

    Install textarea and toaster components

---
+++ src/shared/components/ui/toaster.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/shared/components/ui/toast";
+import { useToast } from "@/shared/hooks/use-toast";
+
+export function Toaster() {
+  const { toasts } = useToast();
+
+  return (
+    <ToastProvider>
+      {toasts.map(({ id, title, description, variant, ...props }) => (
+        <Toast key={id} variant={variant} {...props}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+          <ToastClose />
+        </Toast>
+      ))}
+      <ToastViewport />
+    </ToastProvider>
+  );
+}
